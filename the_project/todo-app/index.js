85d327261900b2c4e0bb052abd547006b376abab
Fei-Linnea <flhaka@utu.fi>
2026-06-24 14:57:07 +0300

    Task 1.5 done

--- the_project/todo-app/index.js
+++ the_project/todo-app/index.js
@@ -3,7 +3,20 @@ const http = require("http");
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(`
+      <html>
+        <body>
+          <h1>Todo App</h1>
+          <p>Project is running.</p>
+        </body>
+      </html>
+    `);
+    return;
+  }
+
+  res.writeHead(404);
   res.end();
 });
 
