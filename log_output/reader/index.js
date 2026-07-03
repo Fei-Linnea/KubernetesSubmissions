874b16d9808ebfe42e8c81acaf39fed247ef4244
Fei-Linnea <flhaka@utu.fi>
2026-07-03 12:40:58 +0300

    Task 1.11 done

--- log_output/reader/index.js
+++ log_output/reader/index.js
@@ -3,22 +3,24 @@ const fs = require("fs");
 
 const PORT = process.env.PORT || 3000;
 const FILE = "/usr/src/app/files/output.txt";
+const COUNTER_FILE = "/usr/src/app/files/counter.txt";
 
 const server = http.createServer((req, res) => {
   if (req.url === "/") {
     try {
-      const content = fs.readFileSync(FILE, "utf8");
-
+      const log = fs.readFileSync(FILE, "utf8").trim();
+      let counter = "0";
+      if (fs.existsSync(COUNTER_FILE)) {
+        counter = fs.readFileSync(COUNTER_FILE, "utf8").trim();
+      }
       res.writeHead(200, {
         "Content-Type": "text/plain",
       });
-
-      res.end(content);
+      res.end(`${log}\nPing / Pongs: ${counter}`);
     } catch {
       res.writeHead(404);
       res.end("No data yet");
     }
-
     return;
   }
 
--- ping_pong/index.js
+++ ping_pong/index.js
@@ -1,14 +1,25 @@
 const http = require("http");
+const fs = require("fs");
+
+const COUNTER_FILE = "/usr/src/app/files/counter.txt";
 
 let counter = 0;
+
+if (fs.existsSync(COUNTER_FILE)) {
+  const data = fs.readFileSync(COUNTER_FILE, "utf8");
+  counter = parseInt(data, 10) || 0;
+}
 
 const port = process.env.PORT || 3001;
 
 const server = http.createServer((req, res) => {
   if (req.url === "/pingpong" && req.method === "GET") {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end(`pong ${counter}`);
     counter++;
+    fs.writeFileSync(COUNTER_FILE, counter.toString());
+    res.writeHead(200, {
+      "Content-Type": "text/plain",
+    });
+    res.end(`pong ${counter}`);
     return;
   }
 
