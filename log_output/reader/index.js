ff39e9ecd8120b439c2ddfb451372e4c692bc628
Fei-Linnea <flhaka@utu.fi>
2026-07-07 14:04:33 +0300

    Task 2.5 done

--- log_output/reader/index.js
+++ log_output/reader/index.js
@@ -3,12 +3,20 @@ const fs = require("fs");
 
 const PORT = process.env.PORT || 3000;
 const FILE = "/usr/src/app/files/output.txt";
+const INFO_FILE = "/usr/src/app/config/information.txt";
 const PINGPONG_URL = "http://pingpong-svc/pings";
 
 const server = http.createServer(async (req, res) => {
   if (req.url === "/") {
     try {
       const log = fs.readFileSync(FILE, "utf8").trim();
+      let info = "";
+      try {
+        info = fs.readFileSync(INFO_FILE, "utf8").trim();
+      } catch {
+        info = "information file missing";
+      }
+      const message = process.env.MESSAGE || "";
       let counter = "0";
       try {
         const response = await fetch(PINGPONG_URL);
@@ -21,14 +29,18 @@ const server = http.createServer(async (req, res) => {
       res.writeHead(200, {
         "Content-Type": "text/plain",
       });
-      res.end(`${log}\nPing / Pongs: ${counter}`);
+      res.end(
+`file content: ${info}
+env variable: MESSAGE=${message}
+${log}
+Ping / Pongs: ${counter}`
+      );
     } catch {
       res.writeHead(404);
       res.end("No data yet");
     }
     return;
   }
-
   res.writeHead(404);
   res.end();
 });
