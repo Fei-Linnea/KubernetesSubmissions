2bdc2a18ae1b9d4c6c700ef75c9ba4d32e00011b
Fei-Linnea <flhaka@utu.fi>
2026-07-26 22:46:58 +0300

    Task 3.4 done

--- ping_pong/index.js
+++ ping_pong/index.js
@@ -4,14 +4,14 @@ let counter = 0;
 const port = process.env.PORT || 3001;
 
 const server = http.createServer((req, res) => {
-  if (req.url === "/" && req.method === "GET") {
+  if (req.url === "/health" && req.method === "GET") {
     res.writeHead(200, {
       "Content-Type": "text/plain",
     });
     res.end("OK");
     return;
   }
-  if (req.url === "/pingpong" && req.method === "GET") {
+  if (req.url === "/" && req.method === "GET") {
     counter++;
     res.writeHead(200, {
       "Content-Type": "text/plain",
